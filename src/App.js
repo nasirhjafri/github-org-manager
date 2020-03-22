1af30172bffebd61ffb99bd9973e334f8c56cfe1
Nasir Hussain <nasirhjafri@gmail.com>
2020-03-22 19:20:20 +0500

    Removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GitHubLogin from 'react-github-login';
-import Button from 'react-bootstrap/Button';
-import Spinner from 'react-bootstrap/Spinner';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 
 import Routes from './routes'
 
-const onSuccess = response => console.log(response);
-const onFailure = response => console.error(response);
 
 
 class App extends React.Component {
